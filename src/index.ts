6c32000fa31a27d91fdac0a608d8eeae4c94e9f5
nub69 <shabik.ashab@gmail.com>
2024-03-23 15:39:47 +0600

    feat: how req works in cloudflare

--- src/index.ts
+++ src/index.ts
@@ -27,6 +27,18 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		// all the necessary information is in the request object
+		console.log(request.body);
+		console.log(request.headers);
+		
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
 	},
 };
